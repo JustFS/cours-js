c3e2133cf264718f6bf7c611c91ee0281bc69852
Julien Az <julien.azbrg@gmail.com>
2021-06-02 15:19:00 +0200

    mousemove

--- index.js
+++ index.js
@@ -18,3 +18,20 @@ closeBtn.addEventListener("click", () => {
   // popup.style.visibility = "hidden";
   popup.classList.remove("popupDisplay");
 });
+
+//--------------------------------------------
+const btn2 = document.getElementById("btn-2");
+const img = document.querySelector("#img");
+
+btn2.addEventListener("click", () => {
+  img.style.filter = "blur(3px) brightness(120%) sepia(70%)";
+});
+
+//--------------------------------------------
+// MOUSEMOVE
+//----------
+
+img.addEventListener("mousemove", (e) => {
+  console.log(e);
+  img.style.left = e.pageX + "px";
+});
