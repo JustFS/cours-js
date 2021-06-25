1e6958e7ca4bf99ba330de169ca4786c42dc83d1
Julien Az <julien.azbrg@gmail.com>
2021-06-25 15:26:54 +0200

    debut js pm

---
+++ passwordMaker/index.js
@@ -0,0 +1,16 @@
+const dataLowercase = "azertyuiopqsdfghjklmwxcvbn";
+const dataUppercase = dataLowercase.toUpperCase();
+const dataNumbers = "1234567890";
+const dataSymbols = "&é\"'(-è_çà)=$^ù!:;,?./§%¨£+";
+
+const generatePassword = () => {
+  let data = [];
+
+  console.log(uppercase.checked);
+
+  data.push(dataLowercase);
+
+  console.log(data);
+};
+
+generateButton.addEventListener("click", generatePassword);
