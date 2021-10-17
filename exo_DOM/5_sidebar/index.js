bda6670bbdf06a2cdd5cf1f02bf94d68551ea233
Julien Az <julien.azbrg@gmail.com>
2021-10-17 11:03:05 +0200

    avant J3

---
+++ exo_DOM/5_sidebar/index.js
@@ -0,0 +1,4 @@
+// Pointer le bouton sur le DOM
+// Ajouter un événement lorsque l'on click dessus
+// Injecter une classe qui révèle la Sidebar
+// Cacher la sidebar si jamais on click sur le contenu principal
