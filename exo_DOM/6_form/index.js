4c921859be0fe0b9f13718bfdd250855311c8edb
Julien Az <julien.azbrg@gmail.com>
2021-10-18 15:26:41 +0200

    form

--- exo_DOM/6_form/index.js
+++ exo_DOM/6_form/index.js
@@ -1,8 +1,60 @@
-// Créer un formulaire qui contiendra :
-// 1 pseudo (min 3 caractère, 16 max)
-// Un input mot de passe
-// Un input confirmer mot de passe (controler que les mots de passes correspondent)
-// Une checkbox pour accepter la charte
-// Un bouton de type submit
+const errorPseudo = document.querySelector(".error-pseudo");
+const errorPassword = document.querySelector(".error-password");
+let pseudoContent;
 
-// Le formulaire ne sera valide que si toutes les conditions sont réunies
+pseudo.addEventListener("input", (e) => {
+  pseudoContent = e.target.value;
+
+  if (pseudoContent.length > 2 && pseudoContent.length < 17) {
+    errorPseudo.textContent = "Pseudo correct";
+    errorPseudo.style.color = "green";
+  } else {
+    errorPseudo.textContent = "Le pseudo doit être entre 3 et 16 caractères";
+    errorPseudo.style.color = "red";
+  }
+});
+
+password.addEventListener("input", (e) => {
+  if (
+    password.value.match(
+      /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/
+    )
+  ) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+});
+
+passwordConfirmation.addEventListener("input", () => {
+  if (password.value === passwordConfirmation.value) {
+    errorPassword.style.color = "green";
+    errorPassword.textContent = "Les mots de passe correspondent";
+  } else {
+    errorPassword.style.color = "red";
+    errorPassword.textContent = "Les mots de passe ne correspondent pas";
+  }
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  console.log();
+
+  if (
+    pseudoContent.length > 2 &&
+    pseudoContent.length < 17 &&
+    password.value === passwordConfirmation.value &&
+    charte.checked
+  ) {
+    alert("BINGO");
+    pseudo.value = "";
+    password.value = "";
+    passwordConfirmation.value = "";
+    charte.checked = false;
+    errorPassword.textContent = "";
+    errorPseudo.textContent = "";
+  } else {
+    alert("Merci de bien renseigner les champs");
+  }
+});
