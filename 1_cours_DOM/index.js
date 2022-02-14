fcbb1561ee219789487af729fabbdc52a107a95f
Julien Az <julien.azbrg@gmail.com>
2022-02-14 12:02:39 +0100

    fin matin J1

--- 1_cours_DOM/index.js
+++ 1_cours_DOM/index.js
@@ -1,12 +1,27 @@
 const title = document.querySelector("h1");
+const btn1 = document.getElementById("btn1");
+const btn2 = document.querySelector(".btn2");
+const btn3 = document.querySelector(".btn3");
+const popup = document.querySelector(".popup");
 
 title.addEventListener("click", () => {
   title.style.fontFamily = "Verdana";
   title.textContent = "Super cours JS";
 });
 
-// Bouton 1 : change le style de l'image
-// Bouton 2 : change le style du body
-// Bouton 3 : fait apparaitre une popup, popup que l'on peut ranger en cliquant sur une croix
+// Ajout classe
+btn1.addEventListener("click", () => {
+  mainImg.classList.toggle("active");
+});
 
-// Pointer les boutons et l'image dans des variables, ajouter des événements sur les boutons
+btn2.addEventListener("click", () => {
+  document.body.style.animation = "animBody 1s";
+});
+
+btn3.addEventListener("click", () => {
+  popup.style.right = "10px";
+});
+
+popupBtn.addEventListener("click", () => {
+  popup.style.right = "-450px";
+});
