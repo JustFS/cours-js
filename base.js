d95f1f1a6391e1ff4810ee339ca4423f70b54071
Julien Az <julien.azbrg@gmail.com>
2022-02-14 10:20:41 +0100

    fin pause J1

---
+++ base.js
@@ -0,0 +1,49 @@
+// Afficher les données
+alert("Yo !");
+console.log(document.body);
+document.body.textContent = "Cours Javascript";
+document.body.innerHTML += "<h1>Cours Javascript</h1>";
+
+//--------------------------------------
+// Variables
+var maVieilleVariable = "Coucou je suis un vielle variable";
+
+// Const = valeur immuable
+const prenom = "julien";
+
+// Let = peut évoluer au fil du code
+let cours = "Ceci est un cours blbalblabalbalabalbal";
+cours = "test";
+
+//------------------------------------
+// Fonction classique
+function faireQuelqueChose() {
+  console.log("Je fais un quelque chose");
+}
+
+faireQuelqueChose();
+
+function addition(a, b) {
+  console.log(a + b);
+}
+addition(21, 484885);
+
+// Struture des fonctions de base
+function nomFonction() {
+  // Algo des choses à faire
+}
+
+const nomDeLaFonction = () => {
+  // Algo des choses à faire
+};
+
+//--------------------------------------
+// Les événements
+
+const maBalise = document.querySelector(".maBalise");
+
+maBalise.addEventListener("type d'événement", fonction);
+
+maBalise.addEventListener("click", () => {
+  // La fonction s'éxecute au click sur la balise
+});
