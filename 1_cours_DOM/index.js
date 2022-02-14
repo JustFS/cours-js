05ddb1b78244499c135c02ea9f148a7903d621c7
Julien Az <julien.azbrg@gmail.com>
2022-02-14 10:45:39 +0100

    exo 1

---
+++ 1_cours_DOM/index.js
@@ -0,0 +1,12 @@
+const title = document.querySelector("h1");
+
+title.addEventListener("click", () => {
+  title.style.fontFamily = "Verdana";
+  title.textContent = "Super cours JS";
+});
+
+// Bouton 1 : change le style de l'image
+// Bouton 2 : change le style du body
+// Bouton 3 : fait apparaitre une popup, popup que l'on peut ranger en cliquant sur une croix
+
+// Pointer les boutons et l'image dans des variables, ajouter des événements sur les boutons
